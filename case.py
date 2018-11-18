5c0f348db6bb27067e7be3f0bb38ed04a32c0987
Shmat00ff <shmat00ff@gmail.com>
2018-11-18 22:43:34 +0700

    Commit

--- case.py
+++ case.py
@@ -3,3 +3,28 @@
 Shmatov D., Bayanova A.
 
 """
+import urllib.request
+
+url = 'http://www.nfl.com/player/jimmygaroppolo/2543801/profile'
+f = urllib.request.urlopen(url)
+s = f.read()
+text = str(s)
+text = text.replace(",", "")
+part_name = text.find("player-name")
+name = text[text.find('>', part_name)+1:text.find('&', part_name)]
+rate = text.find("player-totals") + 50
+Comp = int(text[text.find("<td>", rate)+4:text.find("</td>", rate)])
+Att = int(text[text.find("<td>", rate+50)+4:text.find("</td>", rate+50)])
+Yds = int(text[text.find("<td>", rate+150)+4:text.find("</td>", rate+150)])
+Td = int(text[text.find("<td>", rate+250)+4:text.find("</td>", rate+250)])
+Int = int((text[text.find("<td>", rate+300)+4:text.find("</td>", rate+300)]))
+Rating = ((Comp / Att - 0.3)*5 + (Yds/Att - 3) * 0.25 +(Td/Att)*20 + 2.375 - (Int/Att * 25))/6 * 100
+
+print(Yds)
+print(Att)
+print(Comp)
+print(Td)
+print(Int)
+print(Rating)
+
+print(name)
